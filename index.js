479eeeb3acd2dc7f4873313d6d40d1309eae2230
neillsom <neill.somerville@gmail.com>
2018-09-16 16:30:07 -0700

    test endpoint

--- index.js
+++ index.js
@@ -6,36 +6,48 @@ const morgan = require('morgan');
 
 const { PORT, CLIENT_ORIGIN } = require('./config');
 const { dbConnect } = require('./db-mongoose');
-// const {dbConnect} = require('./db-knex');
 
 const app = express();
 
 app.use(
-  morgan(process.env.NODE_ENV === 'production' ? 'common' : 'dev', {
-    skip: (req, res) => process.env.NODE_ENV === 'test'
-  })
+	morgan(process.env.NODE_ENV === 'production' ? 'common' : 'dev', {
+		skip: (req, res) => process.env.NODE_ENV === 'test'
+	})
 );
 
 app.use(
-  cors({
-    origin: CLIENT_ORIGIN
-  })
+	cors({
+		origin: CLIENT_ORIGIN
+	})
 );
 
+app.get('/api/styles', (req, res) => {
+	const styles = [
+		{name: 'Long Outward Curls With One Side Tucked Behind Ear'},
+		{name: 'Highlighted Messy Updo With Long Side-Swept Bang'},
+		{name: 'Long Layered Bob With Fringes And Razored Ends'},
+		{name: 'Smooth Semi-High Ponytail With Hair Wrap'},
+		{name: 'Messy Low Side Bun With Soft Side-Swept Bang'}
+	];
+
+	res.json(styles);
+});
+
+
 function runServer(port = PORT) {
-  const server = app
-    .listen(port, () => {
-      console.info(`App listening on port ${server.address().port}`);
-    })
-    .on('error', err => {
-      console.error('Express failed to start');
-      console.error(err);
-    });
+	const server = app
+		.listen(port, () => {
+			console.info(`App listening on port ${server.address().port}`);
+		})
+		.on('error', err => {
+			console.error('Express failed to start');
+			console.error(err);
+		});
 }
 
 if (require.main === module) {
-  dbConnect();
-  runServer();
+	dbConnect();
+	runServer();
 }
 
 module.exports = { app };
