4e5233c90023ad8b342bc496b9581fb4e9666eba
Neill Somerville <neill@neillsomerville.com>
2018-09-23 18:28:41 -0700

    styles json, style and user models

---
+++ models/style.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const mongoose = require('mongoose');
+
+const styleSchema = new mongoose.Schema({
+	title: { type: String },
+	content: { type: String },
+	created: { type: Date, default: Date.now },
+	imgUrl: { type: String }
+});
+
+styleSchema.set('toObject', {
+	transform: function (doc, ret) {
+		ret.id = ret._id;
+		delete ret._id;
+		delete ret.__v;
+	}
+});
+
+module.exports = mongoose.model('Style', styleSchema);
---
+++ models/user.js
@@ -0,0 +1,46 @@
+'use strict';
+
+const bcrypt = require('bcryptjs');
+const mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+
+const UserSchema = new mongoose.Schema({
+	fullname: { type: String, default: '' },
+	username: {
+		type: String,
+		required: true,
+		unique: true
+	},
+	password: {
+		type: String,
+		required: true
+	},
+	favorites: {
+		type: Array
+	}
+});
+
+UserSchema.set('toObject', {
+	transform: function(doc, ret) {
+		ret.id = ret._id;
+		delete ret._id;
+		delete ret.__v;
+	}
+});
+
+UserSchema.methods.serialize = function() {
+	return {
+		username: this.username || '',
+	}
+}
+
+UserSchema.methods.validatePassword = function(password) {
+	return bcrypt.compare(password, this.password);
+};
+
+UserSchema.statics.hashPassword = function(password) {
+	return bcrypt.hash(password, 10);
+};
+
+module.exports = mongoose.model('User', UserSchema);
