e92fe053c4b9aa8bf7ea54c1afd6b6eab2dd605f
Neill Somerville <neill@neillsomerville.com>
2018-09-23 22:41:23 -0700

    results shuffled

--- routes/styles.js
+++ routes/styles.js
@@ -7,10 +7,18 @@ const Style = require('../models/style');
 
 const router = express.Router();
 
+function shuffleResults(array) {
+	for (let i = array.length - 1; i > 0; i--) {
+		const j = Math.floor(Math.random() * (i + 1));
+		[array[i], array[j]] = [array[j], array[i]]; // eslint-disable-line no-param-reassign
+	}
+}
+
 // Get all
 router.get('/', (req, res, next) => {
 	Style.find()
 		.then(results => {
+			shuffleResults(results);
 			res.json(results);
 		})
 		.catch(err => {
